d65163832a492ed803e1a921f5a2a8bd618a6e2a
Bstar00 <samueloriola212@gmail.com>
2023-05-19 08:10:42 +0000

    number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,43 +1,53 @@
 #!/usr/bin/python3
-""" add the 5th func that shows  HTML page if n it integer"""
-from flask import Flask, render_template
+""" Add 5th view func that displays HTML page if n is integer """
+
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello():
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    # Replace underscores with spaces
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    # Replace underscores with spaces
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('number_template.html', number=n)
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
